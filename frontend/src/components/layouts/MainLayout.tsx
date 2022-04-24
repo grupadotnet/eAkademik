6f1a4d12b0a379bbf30f6bf4c4dc22c36016aea3
Nieznany4321 <59314982+Nieznany4321@users.noreply.github.com>
2022-04-24 19:27:20 +0200

    Revert "[TASK-8] Added menuItem in navigation folder"

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -1,17 +1,17 @@
 import { Layout } from 'antd';
-import {MenuItem} from '../navigation';
-import { Header } from './components'
-import {Icon} from  '../navigation'
-const { Sider,  Content } = Layout;
+
+import { Header } from './components';
+
+const { Sider, Content } = Layout;
 
 const MainLayout: React.FC = ({ children }) => (
   <Layout style={{ minHeight: '100vh' }}>
-    <Sider width={260}><MenuItem><Icon/>Invoice </MenuItem></Sider>
+    <Sider width={260}>Sider</Sider>
     <Layout>
-      <Header></Header>
+      <Header />
       <Content>{children}</Content>
     </Layout>
   </Layout>
 );
 
-export default MainLayout
+export default MainLayout;
