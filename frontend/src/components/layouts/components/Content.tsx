c5a5752d03300ebcfcfb7b117c24f1c0fe33bb12
00g002 <g.kozyra28@gmail.com>
2022-04-24 17:39:19 +0200

    applied comments from pull request

--- frontend/src/components/layouts/components/Content.tsx
+++ frontend/src/components/layouts/components/Content.tsx
@@ -1,15 +1,8 @@
 import { Layout } from 'antd';
 import styled from 'styled-components';
 
-import { Colors } from '../../../styles';
-
 const { Content: AContent } = Layout;
 
-const Content = styled(AContent)`
-  background-color: ${Colors.onPrimary};
-  box-shadow: 0px 5px 30px -6px rgba(100, 100, 100, 0.5);
-  border-radius: 10px;
-  margin: 25px 20px;
-`;
+const Content = styled(AContent)``;
 
 export default Content;
