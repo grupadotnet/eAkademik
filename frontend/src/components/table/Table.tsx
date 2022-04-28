b4ebc66fcba5781ba7952240f15c671533f9415f
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-04-28 12:18:09 +0200

    Wystylizowalem table

--- frontend/src/components/table/Table.tsx
+++ frontend/src/components/table/Table.tsx
@@ -1,10 +1,28 @@
 import { Table as ATable } from "antd";
 import styled from "styled-components";
 
-import { Colors } from "../../styles";
+import { Colors } from '../../styles';
 
 const Table = styled(ATable)`
-    
+    box-shadow: 0px 5px 30px -6px rgba(100, 100, 100, 0.5);
+    overflow-x: auto;
+    background: ${Colors.onPrimary};
+
+    .ant-table-thead > tr > th {
+        font-weight: 400;
+        color: rgba(0, 0, 0, 0.6);
+        background: ${Colors.onPrimary};
+        border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+    }
+
+    .ant-table-thead > tr > th:not(:last-child):not(.ant-table-selection-column):not(.ant-table-row-expand-icon-cell):not([colspan])::before {
+        display: none;
+    }
+
+    .ant-table-tbody > tr > td {
+        border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+    }
+}
 `;
 
 export default Table;
--- frontend/src/features/utils/views/ComponentsView.tsx
+++ frontend/src/features/utils/views/ComponentsView.tsx
@@ -12,10 +12,60 @@ const Item = styled.div`
   margin: 25px 0px;
 `;
 
+//propsy do tabeli
+
+const columns = [
+  {
+  title: 'Name',
+  key: 'name',
+  dataIndex: 'name'
+},
+{
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+},
+{
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+},
+{
+  title: 'Email',
+  dataIndex: 'email',
+  key: 'email'
+}
+];
+
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+    email: 'mike.stanowski@gmail.com'
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+    email: 'john.deppy@gmail.com'
+  },
+  {
+    key: '3',
+    name: 'Wiktor',
+    age: '20',
+    address: '4 Best Street',
+    email: 'wiktortoszef@gmail.com'
+  }
+]
+
 const ComponentsView = () => (
   <div style={{ padding: 50 }}>
     <Link to="/">Wróć do strony głównej</Link>
     <hr />
+    
     <Item>
       Button component
       <br />
@@ -27,9 +77,13 @@ const ComponentsView = () => (
       Header component
       <Header />
     </Item>
+    
     <Item>
-      <Table pagination={{ pageSize: 20 }}></Table>
+    <br />
+    Table component
+      <Table pagination={{ pageSize: 20 }} columns={columns} dataSource={dataSource} />
     </Item>
+
     <hr/>
   </div>
 );
