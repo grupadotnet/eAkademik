f8fb2eb24f0664905b888e9b825922b514b604ef
unknown <dawid123wilgucki@wp.pl>
2022-05-13 23:51:13 +0200

    Changed how the MenuItem looks

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Layout } from 'antd';
 import {MenuItem} from '../navigation';
 import { Header } from './components'
-import {Icon} from  '../navigation'
+import {ProfileFilled} from '@ant-design/icons';
+
 const { Sider,  Content } = Layout;
 
 const MainLayout: React.FC = ({ children }) => (
   <Layout style={{ minHeight: '100vh' }}>
-    <Sider width={260}><MenuItem><Icon/>Invoice </MenuItem></Sider>
+    <Sider width={260}><MenuItem><ul><ProfileFilled/></ul>Invoice</MenuItem></Sider>
     <Layout>
       <Header></Header>
       <Content>{children}</Content>
