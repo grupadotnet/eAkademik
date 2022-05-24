f3c48c0f6d10c6c066cb6d2d1ccde3c6d582efe7
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-05-24 19:56:43 +0200

    Poprawki

--- frontend/src/features/users/UsersViews.tsx
+++ frontend/src/features/users/UsersViews.tsx
@@ -1,46 +1,26 @@
 import { Table } from "../../components/table";
+import { dataSource } from './mocks/index'
 
 const columns = [
-    {
-      title: "Name",
-      key: "name",
-      dataIndex: "name",
-    },
-    {
-      title: "Surname",
-      dataIndex: "surname",
-      key: "surname",
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
-    },
-  ];
-  
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      surname: 'Stanowski',
-      email: "mike.stanowski@gmail.com",
-    },
-    {
-      key: "2",
-      name: "John",
-      surname: 'Deppy',
-      email: "john.deppy@gmail.com",
-    },
-    {
-      key: "3",
-      name: "Wiktor",
-      surname: 'Rudzki',
-      email: 'wiktortoszef@gmail.com',
-    },
-  ];
+  {
+    title: "Name",
+    key: "name",
+    dataIndex: "name",
+  },
+  {
+    title: "Surname",
+    dataIndex: "surname",
+    key: "surname",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+];
 
 const UsersView = () => (
-    <div style={{padding: "2%"}}>
+    <div>
         <Table dataSource={dataSource} columns={columns} />
     </div>
 );
---
+++ frontend/src/features/users/mocks/UsersDataSource.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    key: "1",
+    name: "Mike",
+    surname: 'Stanowski',
+    email: "mike.stanowski@gmail.com",
+  },
+  {
+    key: "2",
+    name: "John",
+    surname: 'Deppy',
+    email: "john.deppy@gmail.com",
+  },
+  {
+    key: "3",
+    name: "Wiktor",
+    surname: 'Rudzki',
+    email: 'wiktortoszef@gmail.com',
+  },
+];
