037e6da756f3dc1f54796004d72aa2e772bbe483
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-05-25 22:28:04 +0200

    usunalem tesciki ze startupview

--- frontend/src/features/utils/views/StartupView.tsx
+++ frontend/src/features/utils/views/StartupView.tsx
@@ -1,7 +1,5 @@
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { get, remove } from '../../../lib/rest';
 
 const Container = styled.div`
   display: flex;
@@ -11,34 +9,11 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-type User = {
-  id: string;
-  name: string;
-}
-
-const StartupView = () => {
-
-  const getAllUsers = () => {
-    get<User[]>('https://localhost:5001/api/users').then(data => {
-      console.log(data[0].id)
-    })
-  }
-
-  const deleteUser = (id: string) => {
-    remove(`https://localhost:5001/api/users/${id}`)
-  }
-
-useEffect(() => {
-  getAllUsers()
-  deleteUser('3cc29f11-4b32-4b36-8d2f-8c1d65da046e')
-}, [])
-
-  return (
+const StartupView = () => (
     <Container>
       <div>eAkademik</div>
       <div><Link to="/components">sandbox</Link></div>
     </Container>
-  )
-};
+  );
 
 export default StartupView;
