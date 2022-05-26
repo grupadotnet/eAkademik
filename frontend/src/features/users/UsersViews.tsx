aeb8586b36341adb5433d959e64843e45c5f30b4
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-05-26 09:32:10 +0200

    poprawki

--- frontend/src/features/users/UsersViews.tsx
+++ frontend/src/features/users/UsersViews.tsx
@@ -1,28 +1,28 @@
-import { Table } from "../../components/table";
-import { dataSource } from './mocks/index'
+import { Table } from '../../components/table';
+import { usersDataMock } from './mocks';
 
 const columns = [
   {
-    title: "Name",
-    key: "name",
-    dataIndex: "name",
+    title: 'Name',
+    key: 'name',
+    dataIndex: 'name',
   },
   {
-    title: "Surname",
-    dataIndex: "surname",
-    key: "surname",
+    title: 'Surname',
+    dataIndex: 'surname',
+    key: 'surname',
   },
   {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
   },
 ];
 
 const UsersView = () => (
-    <div>
-        <Table dataSource={dataSource} columns={columns} />
-    </div>
+  <div>
+    <Table dataSource={usersDataMock} columns={columns} />
+  </div>
 );
 
 export default UsersView;
--- frontend/src/features/users/mocks/index.ts
+++ frontend/src/features/users/mocks/index.ts
@@ -1,1 +1,1 @@
-export { default as dataSource } from './UsersDataSource';
+export { default as usersDataMock } from './userDataSource';
