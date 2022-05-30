74ab335ade5327f35449c2ebbecae18f1f1b2d76
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-05-30 22:34:12 +0200

    dodalem wstepny widok formularza, ktory musze jeszcze ostylizowac i ogarnac propsy

--- frontend/src/components/form/Form.tsx
+++ frontend/src/components/form/Form.tsx
@@ -1,10 +1,70 @@
-import { Form as AForm, Button, Input, Typography } from 'antd';
+import { Form as AForm, Button, Input, Typography, FormProps } from 'antd';
 
-const Form = () => {
-    const [form] = AForm.useForm();
-    const { Title, Text } = Typography;
+import styled from 'styled-components';
+
+import { Colors } from '@/styles/colors';
+
+const StyledForm = styled(AForm)`
+    box-shadow: 0px 5px 30px -6px ${Colors.shadow};
+    background: ${Colors.onPrimary};
+`
+
+const Form: React.VFC<FormProps<object>> = ({ ...passThroughProps }) => {
+    const [form] = StyledForm.useForm();
+    const { Title } = Typography;
     return (
-        <Title>Adding New User</Title>
+        <div>
+            <Title>Adding New User</Title>
+            <StyledForm
+                form={form}
+                name='add-new-user'
+                layout='vertical'
+            >
+                <StyledForm.Item
+                    label='First name'
+                    name='firstname'
+                    required
+                    tooltip='Required field'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter your first name!',
+                        },
+                    ]}
+                >
+                    <Input placeholder='First Name' />
+                </StyledForm.Item>
+                <StyledForm.Item
+                    label='Last name'
+                    name='lastname'
+                    required
+                    tooltip='Required field'
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter your last name!',
+                        },
+                    ]}
+                >
+                    <Input placeholder='Last Name' />
+                </StyledForm.Item>
+                <StyledForm.Item
+                    label="Email"
+                    name="email"
+                    required
+                    tooltip="This is a required field"
+                    rules={[
+                        {
+                        required: true,
+                        message: "Please enter your email!",
+                        type: "email",
+                        },
+                    ]}
+                    >
+                    <Input placeholder="Email" />
+                </StyledForm.Item>
+            </StyledForm>
+        </div>
     )
 }
 
